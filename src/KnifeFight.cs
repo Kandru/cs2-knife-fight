1f35122252db91f10817579cbf03d11718a3569c
Kalle <kalle@kandru.de>
2025-04-20 15:00:09 +0200

    add panorama vote dependency

--- src/KnifeFight.cs
+++ src/KnifeFight.cs
@@ -1,4 +1,6 @@
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Capabilities;
+using PanoramaVoteManagerAPI;
 
 namespace KnifeFight
 {
@@ -7,11 +9,18 @@ namespace KnifeFight
         public override string ModuleName => "CS2 KnifeFight";
         public override string ModuleAuthor => "Kalle <kalle@kandru.de>";
 
+        private static PluginCapability<IPanoramaVoteManagerAPI> VoteAPI { get; } = new("panoramavotemanager:api");
+        private IPanoramaVoteManagerAPI? _voteManager;
         private bool _isActive = false;
 
         public override void Load(bool hotReload)
         {
             RegisterEventHandler<EventRoundEnd>(OnRoundEnd);
+        }
+
+        public override void OnAllPluginsLoaded(bool hotReload)
+        {
+            _voteManager = VoteAPI.Get();
         }
 
         public override void Unload(bool hotReload)
