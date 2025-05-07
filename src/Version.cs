e0aad30c615eee260e37178eb26bfd172630afcb
Kalle <kalle@kandru.de>
2025-05-07 08:24:48 +0200

    bump version to 0.0.15

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace KnifeFight
 {
     public partial class KnifeFight
     {
-        public override string ModuleVersion => "0.0.14";
+        public override string ModuleVersion => "0.0.15";
     }
 }
