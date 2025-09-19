f8a704a83079901197d9e2ee15b017e4edada74b
derkalle4 <kalle@kandru.de>
2025-09-19 19:07:14 +0200

    add proper knifefight test command (to test whether the API connection is working properly)

--- src/Commands.cs
+++ src/Commands.cs
@@ -2,6 +2,8 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Extensions;
+using PanoramaVoteManagerAPI.Vote;
+using PanoramaVoteManagerAPI.Enums;
 
 namespace KnifeFight
 {
@@ -18,11 +20,39 @@ namespace KnifeFight
                     Config.Reload();
                     command.ReplyToCommand(Localizer["admin.reload"]);
                     break;
+                case "test":
+                    _vote = new(
+                        sfui: Config.SfuiString,
+                        text: new Dictionary<string, string> {
+                            {"en", $"KNIFE FIGHT TEST COMMAND"},
+                        },
+                        time: Config.VoteTime,
+                        team: -1,
+                        playerIDs: [],
+                        initiator: 99,
+                        minSuccessPercentage: 0.51f,
+                        minVotes: 1,
+                        flags: VoteFlags.DoNotEndUntilAllVoted,
+                        callback: CommandTestOnVoteResult
+                    );
+                    int startTime = _voteManager.AddVote(_vote);
+                    command.ReplyToCommand($"vote will start in approx. {startTime} seconds");
+                    break;
                 default:
                     command.ReplyToCommand(Localizer["admin.unknown_command"].Value
                         .Replace("{command}", subCommand));
                     break;
             }
+        }
+
+        public void CommandTestOnVoteResult(Vote vote, bool success)
+        {
+            if (_vote == null)
+            {
+                return;
+            }
+
+            Console.WriteLine($"Vote was {(success ? "successful" : "unsuccessful")} -> {vote.Voters.Count} of {vote.PlayerIDs.Count} have voted");
         }
     }
 }
