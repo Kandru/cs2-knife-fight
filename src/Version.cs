632f01d5fec57709e2414b84813ac82178dd085e
derkalle4 <kalle@kandru.de>
2026-05-31 01:34:50 +0200

    bump version to 26.05.1

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace KnifeFight
 {
     public partial class KnifeFight
     {
-        public override string ModuleVersion => "26.04.1";
+        public override string ModuleVersion => "26.05.1";
     }
 }
